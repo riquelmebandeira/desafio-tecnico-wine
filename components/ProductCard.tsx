afefb0b4d814da14bb00bb7a2b7bfb618f1c520f
riquelme <riquelmebandeira1@outlook.com>
2022-06-30 14:27:32 -0300

    feat: product card component

---
+++ components/ProductCard.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes, { InferProps } from 'prop-types';
+import Button from './Button';
+import Card from '../styles/card';
+
+const propTypes = {
+  image: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  discount: PropTypes.number.isRequired,
+  priceMember: PropTypes.number.isRequired,
+  priceNonMember: PropTypes.number.isRequired,
+};
+
+type ProductCardProps = InferProps<typeof propTypes>;
+
+const ProductCard: React.FC<ProductCardProps> = (props) => {
+  const { image, name, price, discount, priceMember, priceNonMember } = props;
+
+  return (
+    <>
+      <Card>
+        <img src={image} alt="Foto do produto" />
+        <a>{name}</a>
+        <div className="discount-container">
+          <span>{`R$ ${price} `}</span>
+          <span>{`${discount}% Off`}</span>
+        </div>
+        <span className="member-price">Sócio Wine
+          <span className="price-highlight">R$ {priceMember}</span>
+        </span>
+        <span className="non-member-price">{`Não Sócio R$ ${priceNonMember}`}</span>
+      </Card>
+      <Button text="Adicionar" />
+    </>
+  );
+};
+
+ProductCard.propTypes = propTypes;
+
+export default ProductCard;
---
+++ styles/card.ts
@@ -0,0 +1,76 @@
+import styled from 'styled-components';
+
+const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 256px;
+  height: 333px;
+  text-align: center;
+  box-shadow: 0px 9.73384px 14.6008px rgba(0, 0, 0, 0.1);
+  padding: 8px;
+  row-gap: 10px;
+
+  img {
+    width: 198.57px;
+    height: 178.13px;
+    object-fit: contain;
+    align-self: center;
+  }
+
+  a {
+    color: #1D1D1B;
+    font-weight: 700;
+    font-size: 16px;
+    line-height: 19px;
+    cursor: pointer;
+  }
+
+  .discount-container {
+    font-weight: 700;
+    font-size: 10px;
+    line-height: 12px;
+    gap: 8px;
+    display: flex;
+    justify-content: center;
+  }
+
+  .discount-container span:first-child {
+    color: #888888;
+    line-height: 16px;
+    text-align: center;
+    text-decoration-line: line-through;
+  }
+
+  .discount-container span:nth-child(2) {
+    align-self: center;
+    color: white;
+    background: #F79552;
+    border-radius: 1.94677px;
+    text-transform: uppercase;
+    padding: 1.9px;
+  }
+
+  .member-price {
+    color: #1D1D1B;
+    font-weight: 700;
+    font-size: 11px;
+    line-height: 10px;
+    text-transform: uppercase;
+  }
+
+  .non-member-price {
+    color: #888888;
+    font-weight: 700;
+    font-size: 12px;
+    line-height: 14px;
+    text-transform: uppercase;
+  }
+
+  .price-highlight {
+    font-size: 18px;
+    line-height: 20px;
+    color: #B6116E;
+  }
+`;
+
+export default Card;
