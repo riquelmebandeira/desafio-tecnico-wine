90e78860d0371afe97060bdf886cd9daa3d2172b
riquelme <riquelmebandeira1@outlook.com>
2022-06-30 08:50:53 -0300

    feat: add header assets

---
+++ assets/index.ts
@@ -0,0 +1,6 @@
+import searchIcon from './busca.svg';
+import accountIcon from './conta.svg';
+import cartIcon from './carrinho.svg';
+import wineLogo from './logo.svg';
+
+export { searchIcon, accountIcon, cartIcon, wineLogo };
