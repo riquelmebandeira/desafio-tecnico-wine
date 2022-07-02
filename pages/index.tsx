5270e5a9b8b913aa7d5d3c4373f16c538d8013ff
riquelme <riquelmebandeira1@outlook.com>
2022-07-02 14:33:24 -0300

    feat: add product filtering by price

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,22 @@
 import { GetServerSideProps } from 'next';
 import ProductList from '../components/ProductList';
 import Header from '../components/Header';
+import Aside from '../components/Aside';
 
 export default function Home({ products, totalPages }) {
   return (
     <div>
       <Header />
+      <Aside />
       <ProductList products={products} totalPages={totalPages}/>
     </div>
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async ({ query: { page = 1, name } }) => {
-  const url = name ? `https://wine-back-test.herokuapp.com/products?page=${page}&name=${name}&limit=9`
-    : `https://wine-back-test.herokuapp.com/products?page=${page}&limit=9`;
+export const getServerSideProps:
+GetServerSideProps = async ({ query: { page = 1, name, filter } }) => {
+  const url = name ? `https://wine-back-test.herokuapp.com/products?page=${page}&name=${name}&limit=9&filter=${filter}}`
+    : `https://wine-back-test.herokuapp.com/products?page=${page}&limit=9&filter=${filter}`;
 
   const response = await fetch(url);
 
